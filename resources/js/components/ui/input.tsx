151a66590430d8ece027fa8e6c404eac1cdbbbf1
Vladimir Antonio F. C. <50648871+vladiantio@users.noreply.github.com>
2025-02-06 06:28:59 -0300

    style: improve input and label components

--- resources/js/components/ui/input.tsx
+++ resources/js/components/ui/input.tsx
@@ -1,19 +1,37 @@
-import * as React from "react"
-
+import { type ComponentProps, forwardRef } from "react"
 import { cn } from "@/lib/utils"
+import { OctagonAlert } from "lucide-react"
 
-const Input = React.forwardRef<HTMLInputElement, React.ComponentProps<"input">>(
-  ({ className, type, ...props }, ref) => {
+const Input = forwardRef<HTMLInputElement, ComponentProps<"input"> & { error?: string }>(
+  ({ className, type, error, ...props }, ref) => {
     return (
-      <input
-        type={type}
-        className={cn(
-          "flex h-9 w-full rounded-md border border-input bg-transparent px-3 py-1 text-base shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
-          className
+      <>
+        <input
+          type={type}
+          className={cn(
+            "flex h-9 w-full rounded-lg border border-ring/20 bg-background px-3 py-2 text-sm text-foreground shadow-sm shadow-black/5 transition-[border,box-shadow] placeholder:text-muted-foreground/70 hover:border-ring/40 focus-visible:border-ring focus-visible:outline-none focus-visible:ring-[3px] focus-visible:ring-ring/30 disabled:cursor-not-allowed disabled:opacity-50 disabled:border-ring/20",
+            type === "search" &&
+              "[&::-webkit-search-cancel-button]:appearance-none [&::-webkit-search-decoration]:appearance-none [&::-webkit-search-results-button]:appearance-none [&::-webkit-search-results-decoration]:appearance-none",
+            type === "file" &&
+              "p-0 pr-3 italic text-muted-foreground/70 file:me-3 file:h-full file:border-0 file:border-r file:border-solid file:border-input file:bg-transparent file:px-3 file:text-sm file:font-medium file:not-italic file:text-foreground",
+            error && "!border-destructive !ring-[3px] !ring-destructive/30",
+            className,
+          )}
+          ref={ref}
+          {...props}
+        />
+        {error && (
+          <p
+              {...props}
+              className={cn('mt-1 text-sm text-destructive flex items-center', className)}
+              role="alert"
+              aria-live="polite"
+          >
+            <OctagonAlert className="mr-2" size={16} />
+            {error}
+          </p>
         )}
-        ref={ref}
-        {...props}
-      />
+      </>
     )
   }
 )
