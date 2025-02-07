4869eb0cd48a52bf3a5c6504ec5e9c860afd54c7
Vladimir Antonio F. C. <50648871+vladiantio@users.noreply.github.com>
2025-02-07 19:15:54 -0300

    fix: adjust padding and background color for TableRow, TableHead, and TableCell components

--- resources/js/components/ui/table.tsx
+++ resources/js/components/ui/table.tsx
@@ -59,7 +59,7 @@ const TableRow = React.forwardRef<
     ref={ref}
     role={props.onClick ? 'button' : undefined}
     className={cn(
-      "border-b transition-colors data-[state=selected]:bg-muted",
+      "border-b transition-colors data-[state=selected]:bg-primary/5",
       props.onClick && 'hover:bg-muted/50',
       className
     )}
@@ -75,7 +75,7 @@ const TableHead = React.forwardRef<
   <th
     ref={ref}
     className={cn(
-      "h-10 px-2 text-left align-middle font-medium text-muted-foreground [&:has([role=checkbox])]:pr-0 [&>[role=checkbox]]:translate-y-[2px]",
+      "h-10 px-2 text-left align-middle font-medium text-muted-foreground [&:has([role=checkbox])]:pr-1 [&>[role=checkbox]]:translate-y-[2px]",
       className
     )}
     {...props}
@@ -90,7 +90,7 @@ const TableCell = React.forwardRef<
   <td
     ref={ref}
     className={cn(
-      "p-2 align-middle [&:has([role=checkbox])]:pr-0 [&>[role=checkbox]]:translate-y-[2px]",
+      "p-2 align-middle [&:has([role=checkbox])]:pr-1 [&>[role=checkbox]]:translate-y-[2px]",
       className
     )}
     {...props}
